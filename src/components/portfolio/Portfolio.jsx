33c5f506d9beb33a4939f85a56483e08da876ebd
Jaime Hernandez <hernandez.hs@gmail.com>
2022-04-03 21:49:12 -0400

    feat portfolio 4 videos

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import ModalVideo from "react-modal-video";
 import SimpleReactLightbox from "simple-react-lightbox";
-import { SRLWrapper } from "simple-react-lightbox";
 
 const Portfolio = () => {
   const [isOpen, setOpen] = useState(false);
   const [isOpen2, setOpen2] = useState(false);
+  const [isOpen3, setOpen3] = useState(false);
+  const [isOpen4, setOpen4] = useState(false);
   return (
     <SimpleReactLightbox>
       <div className="portfolio_list">
@@ -33,7 +34,7 @@ const Portfolio = () => {
               <img src="https://i.ytimg.com/vi/LJ5LRPdqChE/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAu1NsDWV9YdH75kAZXCSAlRIVsiw" alt="Youtube" />
             </div>
           </li>
-          {/* End youtube */}
+          {/* End youtube 1 */}
           <li>
             <ModalVideo
               channel="youtube"
@@ -57,26 +58,55 @@ const Portfolio = () => {
               <img src="https://i.ytimg.com/vi/c02KviQGgWA/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBMIuR5F8NXIt30gDzgpovGXBKtQw" alt="Youtube" />
             </div>
           </li>
-          {/* End youtube */}
-          
-
-
-          <SRLWrapper>
-            <li>
-              <div className="list_inner">
-                <div className="overlay"></div>
-                <img className="svg" src="img/svg/zoom.svg" alt="" />
-                <div className="title">
-                  <h3>Ave Simone</h3>
-                  <span>Shoot</span>
-                </div>
-
-                <a href="img/portfolio/1.jpg">
-                  <img src="img/portfolio/1.jpg" alt="Light Photography" />
-                </a>
+          {/* End youtube 2 */}
+          <li>
+            <ModalVideo
+              channel="youtube"
+              autoplay
+              isOpen={isOpen3}
+              videoId="vTofmxVcbNU"
+              onClose={() => setOpen3(false)}
+            />
+            {/* End Youtube Modal video */}
+            <div className="list_inner video" onClick={() => setOpen3(true)}>
+              <div className="overlay"></div>
+              <img
+                className="svg popup"
+                src="img/svg/social/youtube.svg"
+                alt="socia"
+              />
+              <div className="title">
+                <h3>IOT Angular Dasboard</h3>
+                <span>Youtube</span>
               </div>
-            </li>
-          </SRLWrapper>
+              <img src="https://i.ytimg.com/vi/vTofmxVcbNU/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBNrgX69zU-FaXD__HkkC_-0FoDng" alt="Youtube" />
+            </div>
+          </li>
+          {/* End youtube 3 */}
+          <li>
+            <ModalVideo
+              channel="youtube"
+              autoplay
+              isOpen={isOpen4}
+              videoId="RwYpQ3e7zbw"
+              onClose={() => setOpen4(false)}
+            />
+            {/* End Youtube Modal video */}
+            <div className="list_inner video" onClick={() => setOpen4(true)}>
+              <div className="overlay"></div>
+              <img
+                className="svg popup"
+                src="img/svg/social/youtube.svg"
+                alt="socia"
+              />
+              <div className="title">
+                <h3>App Encuestas Flutter</h3>
+                <span>Youtube</span>
+              </div>
+              <img src="https://i.ytimg.com/vi/RwYpQ3e7zbw/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCqSr53JZWQwt7JEVM_Kd-zfPru-w" alt="Youtube" />
+            </div>
+          </li>
+          {/* End youtube 4 */}
         </ul>
       </div>
     </SimpleReactLightbox>
